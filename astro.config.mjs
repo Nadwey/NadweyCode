1c44ad1560306cebec15c1f390019e2e6cb8551c
Nadwey <nadweyyy@gmail.com>
2025-08-06 00:13:14 +0200

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,58 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+    site: "https://code.nadwey.pl",
+    vite: {
+        // @ts-expect-error: https://github.com/withastro/astro/issues/14030
+        plugins: [tailwindcss()],
+    },
+    integrations: [
+        starlight({
+            title: "Nadwey Code",
+            components: {
+                PageFrame: "./src/components/overrides/PageFrame.astro",
+                PageTitle: "./src/components/overrides/PageTitle.astro",
+                Pagination: "./src/components/overrides/Pagination.astro",
+                Sidebar: "./src/components/overrides/Sidebar.astro",
+                TwoColumnContent: "./src/components/overrides/TwoColumnContent.astro",
+            },
+            customCss: ["./src/styles/fonts.css", "./src/styles/globals.css", "./src/styles/docs.css"],
+            logo: { src: "./src/assets/img/favicon.png", alt: "Starlight Template" },
+            favicon: "./favicon.png",
+            social: [
+                { icon: "github", label: "GitHub", href: "https://github.com/Nadwey/starlight-template" },
+            ],
+            sidebar: [
+                {
+                    label: "Witaj",
+                    slug: "docs/welcome",
+                },
+                {
+                    label: "Snapshoty",
+                    autogenerate: {
+                        directory: "docs/snapshots",
+                    }
+                },
+            ],
+            expressiveCode: {
+                themes: ["slack-dark", "light-plus"],
+                plugins: [pluginLineNumbers()],
+                defaultProps: {
+                    showLineNumbers: false,
+                    overridesByLang: {
+                        yaml: {
+                            showLineNumbers: true,
+                        },
+                    },
+                },
+            },
+        }),
+        react(),
+    ],
+});
